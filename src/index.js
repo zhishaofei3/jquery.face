ac23acd330ac7ecfff353736e9bd38605e741a5e
kyo4311 <cff20@sina.com>
2016-03-10 16:47:10 +0800

    face

--- src/index.js
+++ src/index.js
@@ -17,49 +17,45 @@
             div.find('.sina-face-head').append(tab);
 
             //触发按钮
-            $(document).on('click', '.sina-face-handle', function(e) {
-                handle = $(this);
-                inputArea = $(this).parents('.sina-face-wrap').find('.sina-face-area');
+            $(document)
+                .on('click', '.sina-face-handle', function(e) {
+                    handle = $(this);
+                    inputArea = $(this).parents('.sina-face-wrap').find('.sina-face-area');
 
-                show(index);
-                e.stopPropagation();
-            });
+                    show(index);
+                    e.stopPropagation();
+                })
+                //Tab切换
+                .on('click', '.sina-face-head a', function(e) {
+                    index = $(this).data('index');
+                    $(this).addClass('current').siblings('a').removeClass();
+                    show(index);
+                    e.stopPropagation();
+                })
+                //点击图标插入
+                .on('click', '.sina-face-body a', function(e) {
+                    var code = $(this).data('code');
+                    insertText(inputArea[0], code);
+                    div.hide();
+                })
+                //阻止关闭
+                .on('click', '.sina-face', function(e) {
+                    e.stopPropagation();
+                })
+                //空白处点击，关闭表情层
+                .on('click', function() {
+                    div.hide();
+                });
 
-
-            //Tab切换
-            $(document).on('click', '.sina-face-head a', function(e) {
-                index = $(this).data('index');
-                $(this).addClass('current').siblings('a').removeClass();
-                show(index);
-                e.stopPropagation();
-            });
-
-
-            //点击图标插入
-            $(document).on('click', '.sina-face-body a', function(e) {
-                var code = $(this).data('code');
-                insertText(inputArea[0], code);
-            });
-
-            //空白处点击，关闭表情层
-            $(document).on('click', function() {
-                div.hide();
-            });
-
-
-            //阻止关才
-            $(document).on('click', '.sina-face', function(e) {
-                e.stopPropagation();
-            });
 
 
             function show() {
+                var i = Math.max(index - 3, 0);
                 var body = create.creatBody(face[index], opt);
                 tab.eq(index).addClass('current').siblings('a').removeClass();
                 div.find('.sina-face-body').html(body);
                 div.show().appendTo('body');
                 position(handle, div);
-                var i = Math.max(index - 3, 0);
                 tab.show().eq(i).prevAll('a').hide();
             }
 
